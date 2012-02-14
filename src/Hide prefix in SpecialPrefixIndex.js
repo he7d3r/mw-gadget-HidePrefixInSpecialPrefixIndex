62f13283ee86992649ce28884ca8dafdf2755f90
Helder <he7d3r@gmail.com>
2012-02-14 17:49:58 +0000

    Hide prefix in [[Special:PrefixIndex]]
    Copied from:
    https://www.mediawiki.org/wiki/Snippets/Hide_prefix_in_SpecialPrefixIndex
    Credits:
    https://www.mediawiki.org/w/index.php?title=Snippets/Hide_prefix_in_SpecialPrefixIndex&action=history
    
    Note: this code was originally hosted on Portuguese Wikibooks (https://pt.wikibooks.org)

---
+++ src/Hide prefix in SpecialPrefixIndex.js
@@ -0,0 +1,46 @@
+/**
+ * Hide prefix in [[Special:PrefixIndex]]
+ *
+ * Adds a checkbox to the form on [[Special:PrefixIndex]].
+ * When the form is submitted with this checkbox ticked,
+ * the prefix value will be hidden in the output (see also Wikimedia's [[bugzilla:27131]]).
+ *
+ * @source: [[mw:Snippets/Hide prefix in SpecialPrefixIndex]]
+ * @rev: 2
+ */
+if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Prefixindex' ) {
+	var prefix = $( '#nsfrom' ).val();
+	var hideprefixVal = mw.util.getParamValue( 'hideprefix' );
+		// ^ Works only 1.17+
+		// Replace with a call to a similar function or create one if you're using 1.16 or lower
+	var $hideprefixLabel = $( '<label/>', {
+			'for': 'hideprefix',
+			'text': 'Hide prefix:'
+	} );
+	var $hideprefixInput = $( '<input/>', {
+		'type': 'checkbox',
+		'name': 'hideprefix',
+		'id': 'hideprefix',
+		'value': '1'
+	} );
+	if ( hideprefixVal == '1' ) {
+		$hideprefixInput.attr( 'checked', 'checked' );
+	}
+	var $hideprefixRow = $( '<tr/>' )
+		.append( $( '<td/>', {
+			'class': 'mw-label',
+			'html': $hideprefixLabel
+			} ) )
+		.append( $( '<td/>', {
+			'class': 'mw-input',
+			'html': $hideprefixInput
+			} ) );
+	// Add checkbox
+	$( '#nsselect' ).find( ' > tbody:first' ).append( $hideprefixRow );
+	// Do it
+	if ( prefix && hideprefixVal == '1' ) {
+		$( '#mw-prefixindex-list-table td a' ).text( function( i, val ) {
+			return val.replace( prefix, '' );
+                } );
+	}
+}
