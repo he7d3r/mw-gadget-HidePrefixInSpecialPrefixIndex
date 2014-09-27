e271d59faf9c25624c8fc6b399b515da05a1d930
Helder <he7d3r@gmail.com>
2014-09-27 20:49:00 -0300

    Apply MediaWiki Coding Conventions
    Validated with jscs 1.6.2, using preset "wikimedia"

--- src/HidePrefixInSpecialPrefixIndex.js
+++ src/HidePrefixInSpecialPrefixIndex.js
@@ -9,53 +9,53 @@
  * @rev: 2
  */
 ( function ( mw, $ ) {
-'use strict';
+	'use strict';
 
-function hidePrefix() {
-	var prefix = $( '#nsfrom' ).val(),
-		hideprefixVal = mw.util.getParamValue( 'hideprefix' ),
-		// ^ Works only 1.17+
-		// Replace with a call to a similar function or create one if you're using 1.16 or lower
-		$hideprefixLabel = $( '<label/>', {
-			'for': 'hideprefix',
-			'text': 'Hide prefix:'
-		} ),
-		$hideprefixInput = $( '<input/>', {
-			'type': 'checkbox',
-			'name': 'hideprefix',
-			'id': 'hideprefix',
-			'value': '1'
-		} ),
-		$hideprefixRow;
-	if ( hideprefixVal === '1' ) {
-		$hideprefixInput.attr( 'checked', 'checked' );
+	function hidePrefix() {
+		var prefix = $( '#nsfrom' ).val(),
+			hideprefixVal = mw.util.getParamValue( 'hideprefix' ),
+			// ^ Works only 1.17+
+			// Replace with a call to a similar function or create one if you're using 1.16 or lower
+			$hideprefixLabel = $( '<label/>', {
+				'for': 'hideprefix',
+				'text': 'Hide prefix:'
+			} ),
+			$hideprefixInput = $( '<input/>', {
+				'type': 'checkbox',
+				'name': 'hideprefix',
+				'id': 'hideprefix',
+				'value': '1'
+			} ),
+			$hideprefixRow;
+		if ( hideprefixVal === '1' ) {
+			$hideprefixInput.attr( 'checked', 'checked' );
+		}
+		$hideprefixRow = $( '<tr/>' )
+			.append( $( '<td/>', {
+				'class': 'mw-label',
+				'html': $hideprefixLabel
+				} ) )
+			.append( $( '<td/>', {
+				'class': 'mw-input',
+				'html': $hideprefixInput
+				} ) );
+		// Add checkbox
+		$( '#nsselect' ).find( ' > tbody:first' ).append( $hideprefixRow );
+		// Do it
+		if ( prefix && hideprefixVal === '1' ) {
+			$( '#mw-prefixindex-list-table' ).find( 'td a' )
+			.add( $( '#mw-prefixindex-list' ).find( 'li a' ) )
+			/*jshint unused:false */
+			.text( function ( i, val ) {
+				return val.replace( prefix, '' );
+			} );
+			/*jshint unused:true */
+		}
 	}
-	$hideprefixRow = $( '<tr/>' )
-		.append( $( '<td/>', {
-			'class': 'mw-label',
-			'html': $hideprefixLabel
-			} ) )
-		.append( $( '<td/>', {
-			'class': 'mw-input',
-			'html': $hideprefixInput
-			} ) );
-	// Add checkbox
-	$( '#nsselect' ).find( ' > tbody:first' ).append( $hideprefixRow );
-	// Do it
-	if ( prefix && hideprefixVal === '1' ) {
-		$( '#mw-prefixindex-list-table' ).find( 'td a' )
-		.add( $( '#mw-prefixindex-list' ).find( 'li a' ) )
-		/*jshint unused:false */
-		.text( function( i, val ) {
-			return val.replace( prefix, '' );
-                } );
-		/*jshint unused:true */
-	}
-}
 
-/* Check if we are in edit mode and the required modules are available and then customize the toolbar */
-if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Prefixindex' ) {
-	$(hidePrefix);
-}
+	/* Check if we are in edit mode and the required modules are available and then customize the toolbar */
+	if ( mw.config.get( 'wgCanonicalSpecialPageName' ) === 'Prefixindex' ) {
+		$(hidePrefix);
+	}
 
 }( mediaWiki, jQuery ) );
